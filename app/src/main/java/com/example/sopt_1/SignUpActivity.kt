c040e617c209cff81c84a2c0da65cc171d37877a
HJinhee <h,wlsgml19@gmail.com>
2021-04-09 14:34:14 +0900

    [FEAT] : 1주차 과제 LEVEL1

---
+++ app/src/main/java/com/example/sopt_1/SignUpActivity.kt
@@ -0,0 +1,87 @@
+package com.example.sopt_1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.example.sopt_1.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignUpBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.d(CURRENT_ACTIVITY, "Called onCreate")
+
+        signUpButtonClickEvent()
+
+    }
+
+    private fun signUpButtonClickEvent() {
+        binding.btnSignUp.setOnClickListener {
+
+            val userName = binding.etSignupName.text
+            val userId = binding.etSignupId.text
+            val userPw = binding.etSignupPw.text
+            if (userName.isNullOrBlank() || userId.isNullOrBlank() || userPw.isNullOrBlank()) {
+                Toast.makeText(this@SignUpActivity, "빈 칸이 있는지 확인해주세요", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                //초기 SignUpActivity로 돌아갈 수 있도록 종료
+                // 종료 전 putExtra를 이용해 모든 값을 intent에 넣어 전달
+                val intent = Intent(this@SignUpActivity, HomeActivity::class.java)
+                intent.putExtra("userName", userName.toString())
+                intent.putExtra("userId", userId.toString())
+                intent.putExtra("userPw", userPw.toString())
+
+                setResult(
+                    RESULT_OK,
+                    intent
+                )   //setResult() 메소드로 결과를 저장 -> 성공 : RESULT_OK, 실패 : RESULT_CANCEL
+                finish()
+            }
+        }
+    }
+
+    companion object {
+        const val CURRENT_ACTIVITY = "SignUpActivity"
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(CURRENT_ACTIVITY, "Called onStart")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(CURRENT_ACTIVITY, "Called onRestart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(CURRENT_ACTIVITY, "Called onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(CURRENT_ACTIVITY, "Called onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(CURRENT_ACTIVITY, "Called onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(CURRENT_ACTIVITY, "Called onDestroy")
+    }
+
+
+}
