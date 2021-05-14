af42c17a9cd25cd9115c6e110a1dd55fdaa306ca
HJinhee <h.wlsgml19@gmail.com>
2021-05-15 00:46:05 +0900

    [MODIFY] RecyclerView 부분 기능 분리

--- app/src/main/java/com/example/sopt_1/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/example/sopt_1/presentation/home/HomeActivity.kt
@@ -40,30 +40,9 @@ class HomeActivity : AppCompatActivity() {
 
 
         initMoreButtonClickEvent()
+        setRepoAdapter()
+        setRepoListData()
 
-        // 1. 우리가 사용할 어뎁터의 초기 값을 넣어준다
-        repositoryListAdapter =
-            RepositoryListAdapter()
-
-        // 2. RecyclerView 에 어뎁터를 우리가 만든 어뎁터로 만들기
-        binding.listRepository.adapter = repositoryListAdapter
-
-        repositoryListAdapter.userList.addAll(
-            listOf<RepositoryInfo>(
-                RepositoryInfo(
-                    repoName = "And-Potter/Ravenclaw_HJinhee",
-                    repoInfo = "진희의 솝트 레포지터리",
-                    repoLanguage = "Kotlin"
-                ),
-                RepositoryInfo(
-                    repoName = "레포지터리 이름이 엄청 길 때 ... 표시되도록 하는거 보여주려고 이렇게 길게 쎄보는 중입니다아",
-                    repoInfo = "레포지터리 설명이 엄청 길 때 ... 표시되도록 하는거 보여주려고 이렇게 길게 쎄보는 중입니다아",
-                    repoLanguage = "Java"
-                )
-            )
-        )
-
-        repositoryListAdapter.notifyDataSetChanged()
 
     }
 
@@ -73,6 +52,33 @@ class HomeActivity : AppCompatActivity() {
             userInfoActivityLauncher.launch(intent)
         }
 
+    }
+
+    private fun setRepoAdapter(){
+        // 1. 우리가 사용할 어뎁터의 초기 값을 넣어준다
+        repositoryListAdapter = RepositoryListAdapter()
+
+        // 2. RecyclerView 에 어뎁터를 우리가 만든 어뎁터로 만들기
+        binding.listRepository.adapter = repositoryListAdapter
+    }
+
+    private fun setRepoListData(){
+        repositoryListAdapter.userList.addAll(
+            listOf<RepositoryInfo>(
+                RepositoryInfo(
+                    repoName = "And-Potter/Ravenclaw_HJinhee",
+                    repoInfo = "진희의 솝트 레포지터리",
+                    repoLanguage = "Kotlin"
+                ),
+                RepositoryInfo(
+                    repoName = "레포지터리 이름이 엄청 길 때 ... 표시되도록 하는거 보여주려고 이렇게 길게 써보는 중입니다아",
+                    repoInfo = "레포지터리 설명이 엄청 길 때 ... 표시되도록 하는거 보여주려고 이렇게 길게 보는 중입니다아",
+                    repoLanguage = "Java"
+                )
+            )
+        )
+
+        repositoryListAdapter.notifyDataSetChanged()
     }
 
 
